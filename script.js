93add8c7133f58e8d22f2e72b31be85cb752de88
Shadi Jaafar <shadinjaafar@gmail.com>
2022-07-17 00:24:27 -0400

    Added 5 day forecast

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const input = document.querySelector('input');
-const enterButton = document.getElementById('enter-button');
+const searchButton = document.getElementById('search-button');
 const celsiusButton = document.getElementById('celsius-button');
 const farenheitButton = document.getElementById('farenheit-button');
+const fiveDayForecast = document.getElementById('five-day-forecast');
 
 const cityName = document.getElementById('city-name');
 const currentWeatherImage = document.getElementById('current-weather-image');
@@ -12,6 +13,7 @@ const windSpeed = document.getElementById('wind-speed');
 const humidity = document.getElementById('humidity');
 
 let weatherData;
+let fiveDayData;
 let country;
 
 async function getWeatherData(cityNameInput, unitSystem, tempUnit, speedUnit) {
@@ -19,6 +21,12 @@ async function getWeatherData(cityNameInput, unitSystem, tempUnit, speedUnit) {
 
     weatherData = await response.json();
     console.log(weatherData);
+
+    const fiveDayResponse = await fetch('https://api.openweathermap.org/data/3.0/onecall?lat=' + weatherData.coord.lat + '&lon=' + weatherData.coord.lon + '&exclude=current,minutely,hourly,alerts&appid=b39b9d581bd43650a4e390d75360effb&units=' + unitSystem, {mode: 'cors'});
+
+    fiveDayData = await fiveDayResponse.json();
+    console.log(fiveDayData);
+    forecastCard(fiveDayData, unitSystem.toString());
 
     feelsLikeTemp.innerText = 'Feels Like \n' + weatherData.main.feels_like.toFixed(0) + tempUnit;
     windSpeed.innerText = 'Wind Speed \n' + weatherData.wind.speed.toFixed(0) + speedUnit;
@@ -32,7 +40,8 @@ async function getWeatherData(cityNameInput, unitSystem, tempUnit, speedUnit) {
 }
 
 
-enterButton.addEventListener('click', () => {
+
+searchButton.addEventListener('click', () => {
     getWeatherData(input.value, 'imperial', ' °F', ' mph').catch(function(error) {
         alert('Could not retrieve data');
     });
@@ -66,4 +75,65 @@ window.onload = function(){
     });
     farenheitButton.style.setProperty('text-decoration', 'underline');
     celsiusButton.style.setProperty('text-decoration', 'none');
+}
+
+
+
+
+function forecastCard(fiveDayData, unitSystem) {
+    reset();
+    if(unitSystem == 'imperial'){ unitSystem = ' °F';} 
+    else {unitSystem = ' °C';}
+    for(let i=0; i<5; i++){
+        const futureDayCard = document.createElement('div');
+
+        if(i == 4){ futureDayCard.style.setProperty('border-right-style', 'none'); }
+        else { futureDayCard.style.setProperty('border-right-style', 'solid'); }
+        
+
+        const weatherImg = document.createElement('img');
+        const lowHighTemp = document.createElement('div');
+        const dayOfWeek = document.createElement('div');
+        const rainImg = document.createElement('img');
+        const chanceOfRain = document.createElement('div');
+        
+        dayOfWeek.innerText = getDay(fiveDayData.daily[i+1].dt);
+
+        weatherImg.style.setProperty('margin-left', '12px');
+
+        weatherImg.src = "http://openweathermap.org/img/wn/" + fiveDayData.daily[i].weather[0].icon + "@2x.png" 
+        lowHighTemp.innerText = 'High | Low \n' + fiveDayData.daily[i].temp.max.toFixed(0) + ' | ' + fiveDayData.daily[i].temp.min.toFixed(0) + unitSystem;
+        
+        rainImg.src = "./Icons/Rain.png";
+        rainImg.style.setProperty('height', '25px');
+        rainImg.style.setProperty('margin', '5px 10px 0px 0px');
+        chanceOfRain.style.setProperty('font-size', '25px');
+
+        
+        chanceOfRain.appendChild(rainImg);
+        chanceOfRain.innerHTML += (fiveDayData.daily[i].pop * 100).toFixed(0) + '%';
+        chanceOfRain.style.setProperty('text-align', 'center');
+
+        futureDayCard.classList.add('future-day-card');
+        futureDayCard.appendChild(dayOfWeek);
+        futureDayCard.appendChild(weatherImg);
+        futureDayCard.appendChild(lowHighTemp);
+        futureDayCard.appendChild(chanceOfRain);
+
+        fiveDayForecast.appendChild(futureDayCard);
+    }
+}
+
+function reset(){
+    const forecastArray = Array.from(fiveDayForecast.childNodes);
+    forecastArray.forEach((element) => {
+        fiveDayForecast.removeChild(element);
+    });
+}
+
+
+function getDay(timestamp) {
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const date = new Date(timestamp * 1000);
+    return weekday[date.getDay()];
 }
